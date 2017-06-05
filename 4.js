3358a8bb6135361328aca2ea213d76e526bdd815
cwebsterz <cwebsterzornes@live.com>
2017-06-05 18:10:59 -0400

    updated 4

--- 4.js
+++ 4.js
@@ -1,9 +1,9 @@
 // Problem 4
 
-var stringItem = 'Do the best you can until you know better. Then, when you know better, do better'
+var faveQuote = 'Do the best you can until you know better. Then, when you know better, do better'
 
 function stringParameterize (item) {
   return item.toLowerCase().split(' ').join('-');
 }
 
-console.log(stringParameterize(stringItem))
+console.log(stringParameterize(faveQuote))
